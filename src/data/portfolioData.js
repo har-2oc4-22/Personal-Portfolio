aea960ac01f6d392c02d2765d960f6827d1e4ed3
Harsh Shukla <136715205+har-2oc4-22@users.noreply.github.com>
2026-05-19 23:07:17 +0530

    Update portfolioData.js

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -19,7 +19,7 @@ export const personalInfo = {
     email: "shuklaharsh9752@gmail.com",
     github: "https://github.com/har-2oc4-22",
     linkedin: "https://www.linkedin.com/in/harsh-shukla-a83b3b231/",
-    resumeLink: "https://drive.google.com/file/d/1fXjNmgej5v3xy_5NSUVVq3Q4kf0HqOiJ/view?usp=sharing",
+    resumeLink: "https://drive.google.com/file/d/1wJswYQYbXbV3O9T57HbNxCrJuYZkfFng/view?usp=sharing",
     about: "I am a final year Computer Science engineering student at the Institute Of Engineering And Technology, Lucknow. As a Full-Stack Developer with strong DSA and Competitive Programming skills, I am proficient in building scalable applications featuring secure authentication, integrated payments, and media management. I'm passionate about writing clean, efficient code and continuously learning new technologies.",
 };
 
